d68dde7a7a2a0daddf7d4415b32a87d965307511
Alex Potter <alex364981@gmail.com>
2014-02-08 20:46:00 -0800

    Birthday
    Oops :)

---
+++ Birthday/Form1.Designer.vb
@@ -0,0 +1,148 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.lblSeconds = New System.Windows.Forms.Label()
+        Me.lblMinutes = New System.Windows.Forms.Label()
+        Me.lblHours = New System.Windows.Forms.Label()
+        Me.lblDays = New System.Windows.Forms.Label()
+        Me.lblHeader = New System.Windows.Forms.Label()
+        Me.Countdown = New System.Windows.Forms.Timer(Me.components)
+        Me.lblHalfSeconds = New System.Windows.Forms.Label()
+        Me.picFireworks = New System.Windows.Forms.PictureBox()
+        CType(Me.picFireworks, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'lblSeconds
+        '
+        Me.lblSeconds.BackColor = System.Drawing.Color.Transparent
+        Me.lblSeconds.ForeColor = System.Drawing.Color.White
+        Me.lblSeconds.Location = New System.Drawing.Point(345, 123)
+        Me.lblSeconds.Name = "lblSeconds"
+        Me.lblSeconds.Size = New System.Drawing.Size(100, 23)
+        Me.lblSeconds.TabIndex = 11
+        Me.lblSeconds.Text = "{Seconds}"
+        Me.lblSeconds.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblMinutes
+        '
+        Me.lblMinutes.BackColor = System.Drawing.Color.Transparent
+        Me.lblMinutes.ForeColor = System.Drawing.Color.White
+        Me.lblMinutes.Location = New System.Drawing.Point(345, 100)
+        Me.lblMinutes.Name = "lblMinutes"
+        Me.lblMinutes.Size = New System.Drawing.Size(100, 23)
+        Me.lblMinutes.TabIndex = 10
+        Me.lblMinutes.Text = "{Minutes}"
+        Me.lblMinutes.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblHours
+        '
+        Me.lblHours.BackColor = System.Drawing.Color.Transparent
+        Me.lblHours.ForeColor = System.Drawing.Color.White
+        Me.lblHours.Location = New System.Drawing.Point(345, 77)
+        Me.lblHours.Name = "lblHours"
+        Me.lblHours.Size = New System.Drawing.Size(100, 23)
+        Me.lblHours.TabIndex = 9
+        Me.lblHours.Text = "{Hours}"
+        Me.lblHours.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblDays
+        '
+        Me.lblDays.BackColor = System.Drawing.Color.Transparent
+        Me.lblDays.ForeColor = System.Drawing.Color.White
+        Me.lblDays.Location = New System.Drawing.Point(345, 54)
+        Me.lblDays.Name = "lblDays"
+        Me.lblDays.Size = New System.Drawing.Size(100, 23)
+        Me.lblDays.TabIndex = 8
+        Me.lblDays.Text = "{Days}"
+        Me.lblDays.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblHeader
+        '
+        Me.lblHeader.Font = New System.Drawing.Font("Microsoft Sans Serif", 21.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblHeader.ForeColor = System.Drawing.Color.White
+        Me.lblHeader.Location = New System.Drawing.Point(12, 0)
+        Me.lblHeader.Name = "lblHeader"
+        Me.lblHeader.Size = New System.Drawing.Size(766, 38)
+        Me.lblHeader.TabIndex = 7
+        Me.lblHeader.Text = "Birthday Countdown"
+        Me.lblHeader.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Countdown
+        '
+        Me.Countdown.Enabled = True
+        '
+        'lblHalfSeconds
+        '
+        Me.lblHalfSeconds.BackColor = System.Drawing.Color.Transparent
+        Me.lblHalfSeconds.ForeColor = System.Drawing.Color.White
+        Me.lblHalfSeconds.Location = New System.Drawing.Point(345, 146)
+        Me.lblHalfSeconds.Name = "lblHalfSeconds"
+        Me.lblHalfSeconds.Size = New System.Drawing.Size(100, 23)
+        Me.lblHalfSeconds.TabIndex = 12
+        Me.lblHalfSeconds.Text = "{HalfSeconds}"
+        Me.lblHalfSeconds.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'picFireworks
+        '
+        Me.picFireworks.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.picFireworks.Image = CType(resources.GetObject("picFireworks.Image"), System.Drawing.Image)
+        Me.picFireworks.Location = New System.Drawing.Point(0, 0)
+        Me.picFireworks.Name = "picFireworks"
+        Me.picFireworks.Size = New System.Drawing.Size(790, 250)
+        Me.picFireworks.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picFireworks.TabIndex = 13
+        Me.picFireworks.TabStop = False
+        Me.picFireworks.Visible = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Black
+        Me.ClientSize = New System.Drawing.Size(790, 250)
+        Me.Controls.Add(Me.lblSeconds)
+        Me.Controls.Add(Me.lblMinutes)
+        Me.Controls.Add(Me.lblHours)
+        Me.Controls.Add(Me.lblDays)
+        Me.Controls.Add(Me.lblHeader)
+        Me.Controls.Add(Me.lblHalfSeconds)
+        Me.Controls.Add(Me.picFireworks)
+        Me.Name = "Form1"
+        Me.Text = "Birthday Countdown"
+        CType(Me.picFireworks, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents lblSeconds As System.Windows.Forms.Label
+    Friend WithEvents lblMinutes As System.Windows.Forms.Label
+    Friend WithEvents lblHours As System.Windows.Forms.Label
+    Friend WithEvents lblDays As System.Windows.Forms.Label
+    Friend WithEvents lblHeader As System.Windows.Forms.Label
+    Friend WithEvents Countdown As System.Windows.Forms.Timer
+    Friend WithEvents lblHalfSeconds As System.Windows.Forms.Label
+    Friend WithEvents picFireworks As System.Windows.Forms.PictureBox
+
+End Class
---
+++ Birthday/Form1.vb
@@ -0,0 +1,95 @@
+﻿Public Class Form1
+    Public currentDate As DateTime = DateTime.Now
+    Dim ShowLBL As Boolean = True
+    Private Sub Countdown_Tick(sender As Object, e As EventArgs) Handles Countdown.Tick
+        'Starts the Days Function
+        Days()
+        'Starts the Hours Function
+        Hours()
+        'Starts the Minutes Function
+        Minutes()
+        'Starts the Seconds Function
+        Seconds()
+        'Starts the MilliSeconds Function
+        MilliSeconds()
+        'Keeps the labels up to date with the current times
+        lblDays.Text = Days() & " Days"
+        lblHours.Text = Hours() & " Hours"
+        lblMinutes.Text = Minutes() & " Minutes"
+        lblSeconds.Text = Seconds() & " Seconds"
+        lblHalfSeconds.Text = MilliSeconds() & " MilliSeconds"
+        'Starts the text change sub procedure
+        TextChange()
+        'If the day matches, then it turns on the BirthdayOn Sub Procedure
+        If currentDate.Month = 2 Then
+            If currentDate.Day = 8 Then
+                BirthdayOn()
+            End If
+        End If
+    End Sub
+    Sub BirthdayOn()
+        'Turns on the Fireworks
+        picFireworks.Show()
+        If ShowLBL Then
+            'Turns off the rest of the sub procedure
+            ShowLBL = False
+            'Activates the rest of this code
+            lblHeader.Text = "Happy Birthday!"
+            'Hides the time labels
+            lblDays.Hide()
+            lblHalfSeconds.Hide()
+            lblHours.Hide()
+            lblMinutes.Hide()
+            lblSeconds.Hide()
+            MsgBox("Happy 18th Birthday!")
+        Else
+            'Changes the title of the form
+            Title()
+        End If
+    End Sub
+    Sub Title()
+        Me.Text = "HAPPY BIRTHDAY!"
+    End Sub
+    Function Days() As Integer
+        'Get the current Date
+        Dim thisDay As Integer = Microsoft.VisualBasic.DateAndTime.Day(Now)
+        Dim BirthDate As Integer
+        If currentDate.Month = 1 Then
+            BirthDate = 31 - System.DateTime.Now.Day + 8
+        ElseIf currentDate.Month = 2 Then
+            If currentDate.Day = 8 Then
+                BirthDate = 8 - System.DateTime.Now.Day
+            ElseIf currentDate.Day >= 9 Then
+                MsgBox("It is past your birthday! Sorry!")
+            End If
+        Else
+            MsgBox("Past your birthday month! Hope you have another good year!")
+        End If
+            Return BirthDate
+    End Function
+    Function Hours() As Integer
+        'Get the Current Hour
+        Dim HoursInt As Integer = 24 - System.DateTime.Now.Hour
+        Return HoursInt
+    End Function
+    Function Minutes() As Integer
+        'Gets the Current Minutes
+        Dim MinutesInt As Integer = 60 - System.DateTime.Now.Minute
+        Return MinutesInt
+    End Function
+    Function Seconds() As Integer
+        'Get the Current Seconds
+        Dim SecondsInt As Integer = 60 - System.DateTime.Now.Second
+        Return SecondsInt
+    End Function
+    Function MilliSeconds() As Integer
+        'Gets the Current MilliSeconds
+        Dim MilliInt As Integer = 1000 - System.DateTime.Now.Millisecond
+        Return MilliInt
+    End Function
+    Sub TextChange()
+        'Changes the Form Text
+        'Changes it to match the time per Timer Tick
+        Me.Text = "Birthday Countdown | " & Days() & ":" & Hours() & ":" & Minutes() & ":" & Seconds() & ":" & MilliSeconds()
+    End Sub
+End Class
